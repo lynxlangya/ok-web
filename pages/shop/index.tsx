108e3a666d6624f6955f9782baaa58ff381c97d6
山人自有妙计 <wangdaoo@yeah.net>
2021-11-04 11:01:42 +0800

    feat: 界面展示出来先 :rocket:

--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -1,12 +1,24 @@
 import Layout from '@/components/Layout';
 import type { NextPage } from 'next';
+import MasterTitle from '@/components/MasterTitle'
+import styles from '@/styles/shop/title.module.less';
+import { message } from 'antd'
 
 const About: NextPage = (props: any) => {
   return (
     <Layout>
-      <main>商城</main>
+      <section className={styles.container}>
+        <MasterTitle title="商城" subTitle="Shop" isSearch onSearch={_handleSearch} />
+        <div className={styles.article_box}>
+          渲染数据就完事
+        </div>
+      </section>
     </Layout>
   );
 };
+
+function _handleSearch(value: string) {
+  message.info(value);
+}
 
 export default About;
